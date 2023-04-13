0d15251cc179b28ebc3634937c4637ec55897578
Dr V <research@drscovetta.info>
2023-04-13 11:55:19 -0400

    Create test001.asm

---
+++ test001.asm
@@ -0,0 +1,11 @@
+***************************************************** 
+* Program Name: Test001 
+* Program Author: Dr. Scovetta
+* Program Description: Illustrate Write To Operator
+***************************************************** 
+        TITLE 'Z390 Test001 HELLO WORLD 1'
+        PRINT  ON,GEN,DATA
+TEST001 SUBENTRY
+        WTO   'TEST001 HELLO WORLD 1' 	
+        SUBEXIT
+        END
