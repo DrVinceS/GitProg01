57184eb580a6a4c1cc1aec9611d86ab9723169d4
Dr V <research@drscovetta.info>
2023-04-13 11:58:48 -0400

    Update test001.asm

--- test001.asm
+++ test001.asm
@@ -3,9 +3,10 @@
 * Program Author: Dr. Scovetta
 * Program Description: Illustrate Write To Operator
 ***************************************************** 
-        TITLE 'Z390 Test001 HELLO WORLD 1'
-        PRINT  ON,GEN,DATA
+        TITLE 	'Z390 Test001 HELLO WORLD 1'
+        PRINT  	ON,GEN,DATA
 TEST001 SUBENTRY
-        WTO   'TEST001 HELLO WORLD 1' 	
+        WTO		'TEST001 HELLO WORLD 1' 	
+		WTO 	'TEST001 HELLO WORLD 2' 	
         SUBEXIT
-        END
+        END		TEST00
